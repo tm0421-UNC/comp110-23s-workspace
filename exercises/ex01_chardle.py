e3dd08506f3772b845947c9d3622cf6670c9052b
Tyler McCune <tm0421@email.unc.edu>
2023-01-19 18:42:10 -0500

    Finished EX01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,21 +3,23 @@
 __author__ = "730391892"
 
 chardle_word: str = input("Enter a 5-character word: ")
-if len(chardle_word)!=5:
+if len(chardle_word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
 char_guess: str = input("Enter a single character: ")
-if len(char_guess)!=1:
+if len(char_guess) != 1:
     print("Error: Character must be a single character")
     exit()
 print("Searching for " + char_guess + " in " + chardle_word)
 
 for i in range(len(chardle_word)):
     if(chardle_word[i] == char_guess):
-      print(char_guess+" found at index "+ str(i))
+        print(char_guess + " found at index " + str(i))
 
 num_instances: int=sum(char==char_guess for char in chardle_word)
-if num_instances!=0:
-    print(str(num_instances)+" instances of "+char_guess+" found in "+chardle_word)
+if num_instances == 1:
+    print(str(num_instances) + " instance of " + char_guess + " found in " + chardle_word)
+    if num_instances > 1:
+        print(str(num_instances) + " instances of " + char_guess + " found in " + chardle_word)
 else:
-    print("No instances of "+char_guess+" found in "+chardle_word)
+    print("No instances of " + char_guess + " found in " + chardle_word)
