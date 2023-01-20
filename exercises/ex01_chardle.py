dcc4da8aa952aa5a82ab0b439d8d30b7d30b246d
Tyler McCune <tm0421@email.unc.edu>
2023-01-20 11:17:53 -0500

    Finished EX01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,24 +2,24 @@
 
 __author__ = "730391892"
 
-keyword: str = input("Enter a 5-character word: ")
-if len(keyword) != 5:
+key_word: str = input("Enter a 5-character word: ")
+if len(key_word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
 char_guess: str = input("Enter a single character: ")
 if len(char_guess) != 1:
     print("Error: Character must be a single character")
     exit()
-print("Searching for " + char_guess + " in " + keyword)
+print("Searching for " + char_guess + " in " + key_word)
 
-for i in range(len(keyword)):
-    if keyword[i] == char_guess:
+for i in range(len(key_word)):
+    if key_word[i] == char_guess:
         print(char_guess + " found at index " + str(i))
 
-num_instances: int = sum(char == char_guess for char in keyword)
+num_instances: int = sum(char == char_guess for char in key_word)
 if num_instances == 1:
-    print(str(num_instances) + " instance of " + char_guess + " found in " + keyword)
+    print(str(num_instances) + " instance of " + char_guess + " found in " + key_word)
 elif num_instances > 1:
-    print(str(num_instances) + " instances of " + char_guess + " found in " + keyword)
+    print(str(num_instances) + " instances of " + char_guess + " found in " + key_word)
 else:
-    print("No instances of " + char_guess + " found in " + keyword)
+    print("No instances of " + char_guess + " found in " + key_word)
