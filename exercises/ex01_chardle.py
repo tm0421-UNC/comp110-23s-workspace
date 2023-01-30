adaa408b5ae4c5272b558913373738d8ed79df87
Tyler McCune <tm0421@email.unc.edu>
2023-01-30 11:27:14 -0500

    Finished EX01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -12,15 +12,32 @@ if len(char_guess) != 1:
     exit()
 print("Searching for " + char_guess + " in " + key_word)
 
-for x in range(len(key_word)):
-    if key_word[x] == char_guess:
-        print(char_guess + " found at index " + str(x))
+num_instances: int = 0
+if char_guess == key_word[0]:
+    print(char_guess + " found at index 0")
+    num_instances = num_instances + 1
 
-num_instances: int = sum(char == char_guess for char in key_word)
+if char_guess == key_word[1]:
+    print(char_guess + " found at index 1")
+    num_instances = num_instances + 1
+
+if char_guess == key_word[2]:
+    print(char_guess + " found at index 2")
+    num_instances = num_instances + 1
+
+if char_guess == key_word[3]:
+    print(char_guess + " found at index 3")
+    num_instances = num_instances + 1
+
+if char_guess == key_word[4]:
+    print(char_guess + " found at index 4")
+    num_instances = num_instances + 1
+
+if num_instances == 0:
+    print("No instances of " + char_guess + " found in " + key_word)
+
 if num_instances == 1:
-    print(str(num_instances) + " instance of " + char_guess + " found in " + key_word)
-else: 
-    if num_instances > 1:
-        print(str(num_instances) + " instances of " + char_guess + " found in " + key_word)
-    else:
-        print("No instances of " + char_guess + " found in " + key_word)
+    print("1 instance of " + char_guess + " found in " + key_word)
+
+if num_instances > 1:
+    print(str(num_instances) + " instances of " + char_guess + " found in " + key_word)
