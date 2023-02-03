410ccc0f25540d95004b091d5f1ee710559f667a
Tyler McCune <tm0421@email.unc.edu>
2023-02-02 19:11:33 -0500

    Finished EX02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -9,30 +9,37 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 idx: int = 0
-index_emoji: str = ""
+emoji_str: str = ""
 char_elsewhere: bool = False
 alt_idx: int = 0
 
+#check that the guess is the same length as the secret word
 while int(len(guess)) != int(len(secret_word)):
     guess = input(f"That was not {word_len} letters! Try again: ")
 
+#look at each character in the guess
 while idx < len(secret_word):
+    #check if the guess and the secret word have the same character at the same index
     if guess[idx] == secret_word[idx]:
-        index_emoji = index_emoji + GREEN_BOX
+        #update the emoji string to show correct character and index
+        emoji_str = emoji_str + GREEN_BOX
     else: 
+        #check if a guessed character occurs elsewhere in the secret word
         while (char_elsewhere == False and alt_idx < len(secret_word)):
             if guess[idx] == secret_word[alt_idx]:
                 char_elsewhere = True
             alt_idx = alt_idx + 1
+        #update the emoji string based on if the guessed character appears in the secret word
         if char_elsewhere == True:
-            index_emoji = index_emoji + YELLOW_BOX
+            emoji_str = emoji_str + YELLOW_BOX
         else: 
-            index_emoji = index_emoji + WHITE_BOX
+            emoji_str = emoji_str + WHITE_BOX
     alt_idx = 0
     char_elsewhere = False
     idx = idx + 1
-print(index_emoji)
+print(emoji_str)
 
+#check if the guess is the secret word
 if guess == secret_word:
     print("Woo! You got it! ")
 else:
